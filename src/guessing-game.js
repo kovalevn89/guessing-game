55fdd798a2f114c7c7b06ed0d04d503a980f9250
Niklay Kovalev <kovalev.nikolay.89@gmail.com>
2022-08-10 23:17:08 +0300

    feat: add guessing game function

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,27 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.min = 0;
+        this.max = 0;
+        this.candidate = 0;
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        this.candidate = Math.round(this.min + (this.max - this.min) / 2);
+        return this.candidate;
     }
 
     lower() {
-
+        this.max = this.candidate;
     }
 
     greater() {
-
+        this.min = this.candidate;
     }
-}
+};
 
 module.exports = GuessingGame;
